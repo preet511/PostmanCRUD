022f7269b547b669a26f67d235b9454b3be4dae9
preet511 <73891944+preet511@users.noreply.github.com>
2021-07-04 21:12:42 +0530

    Delete src directory

--- src/main/java/com/example/SpringBoot/repository/CRUDrepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringBoot.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.SpringBoot.model.Employee;
-
-public interface CRUDrepo extends CrudRepository<Employee, Integer>{
-
-}
